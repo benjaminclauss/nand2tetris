3ee9aa8c07cffa2da7f70b00e80b14a29c35a6ef
Benjamin Clauss <benjaminclauss@protonmail.com>
2024-07-26 17:08:11 -0400

    Add helpers for arithmetic methods

--- 7/virtualmachine/code_writer.go
+++ 7/virtualmachine/code_writer.go
@@ -3,6 +3,7 @@ package virtualmachine
 import (
 	"io"
 	"strconv"
+	"strings"
 )
 
 // A CodeWriter translates VM commands into Hack assembly code.
@@ -26,74 +27,42 @@ func (cw *CodeWriter) SetFileName(filename string) {}
 // WriteArithmetic writes the assembly code that is the translation of the given arithmetic command.
 func (cw *CodeWriter) WriteArithmetic(command string) {
 	switch command {
-	case "add":
-		// Decrement stack pointer.
-		io.WriteString(cw.output, "@SP\nM=M-1\n")
-		// Store first operand.
-		io.WriteString(cw.output, "A=M\nD=M\n")
-		// Operate.
-		io.WriteString(cw.output, "A=A-1\n")
-		io.WriteString(cw.output, "M=D+M\n")
-	case "sub":
-		// Decrement stack pointer.
-		io.WriteString(cw.output, "@SP\nM=M-1\n")
-		// Store first operand.
-		io.WriteString(cw.output, "A=M\nD=M\n")
-		// Operate.
-		io.WriteString(cw.output, "A=A-1\n")
-		io.WriteString(cw.output, "M=M-D\n")
-	case "neg":
-		io.WriteString(cw.output, "@SP\nA=M\n")
-		io.WriteString(cw.output, "A=A-1\n")
-		io.WriteString(cw.output, "M=-M\n")
-	case "eq":
-		cw.boolean += 1
-		comp := "EQ"
-		count := strconv.Itoa(cw.boolean)
-		io.WriteString(cw.output, "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n"+
-			"@"+comp+".true."+count+"\nD;J"+comp+"\n"+
-			"@SP\nA=M-1\nM=0\n@"+comp+".after."+count+"\n"+
-			"0;JMP\n("+comp+".true."+count+")\n@SP\nA=M-1\n"+
-			"M=-1\n("+comp+".after."+count+")\n")
-	case "gt":
-		cw.boolean += 1
-		comp := "GT"
-		count := strconv.Itoa(cw.boolean)
-		io.WriteString(cw.output, "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n"+
-			"@"+comp+".true."+count+"\nD;J"+comp+"\n"+
-			"@SP\nA=M-1\nM=0\n@"+comp+".after."+count+"\n"+
-			"0;JMP\n("+comp+".true."+count+")\n@SP\nA=M-1\n"+
-			"M=-1\n("+comp+".after."+count+")\n")
-	case "lt":
-		cw.boolean += 1
-		comp := "LT"
-		count := strconv.Itoa(cw.boolean)
-		io.WriteString(cw.output, "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n"+
-			"@"+comp+".true."+count+"\nD;J"+comp+"\n"+
-			"@SP\nA=M-1\nM=0\n@"+comp+".after."+count+"\n"+
-			"0;JMP\n("+comp+".true."+count+")\n@SP\nA=M-1\n"+
-			"M=-1\n("+comp+".after."+count+")\n")
-	case "and":
-		// Decrement stack pointer.
-		io.WriteString(cw.output, "@SP\nM=M-1\n")
-		// Store first operand.
-		io.WriteString(cw.output, "A=M\nD=M\n")
-		// Operate.
-		io.WriteString(cw.output, "A=A-1\n")
-		io.WriteString(cw.output, "M=D&M\n")
-	case "or":
-		// Decrement stack pointer.
-		io.WriteString(cw.output, "@SP\nM=M-1\n")
-		// Store first operand.
-		io.WriteString(cw.output, "A=M\nD=M\n")
-		// Operate.
-		io.WriteString(cw.output, "A=A-1\n")
-		io.WriteString(cw.output, "M=D|M\n")
-	case "not":
-		io.WriteString(cw.output, "@SP\nA=M\n")
-		io.WriteString(cw.output, "A=A-1\n")
-		io.WriteString(cw.output, "M=!M\n")
+	case "add", "sub", "and", "or":
+		cw.writeBinaryOperation(command)
+	case "neg", "not":
+		cw.writeUnaryOperation(command)
+	case "eq", "gt", "lt":
+		cw.writeComparison(command)
 	}
+}
+
+func (cw *CodeWriter) writeBinaryOperation(command string) {
+	operation := map[string]string{"add": "D+M", "sub": "M-D", "and": "D&M", "or": "D|M"}[command]
+	// Decrement stack pointer.
+	io.WriteString(cw.output, "@SP\nM=M-1\n")
+	// Store first operand.
+	io.WriteString(cw.output, "A=M\nD=M\n")
+	// Operate.
+	io.WriteString(cw.output, "A=A-1\n")
+	io.WriteString(cw.output, "M="+operation+"\n")
+}
+
+func (cw *CodeWriter) writeUnaryOperation(command string) {
+	result := map[string]string{"neg": "-M", "not": "!M"}[command]
+	io.WriteString(cw.output, "@SP\nA=M\n")
+	io.WriteString(cw.output, "A=A-1\n")
+	io.WriteString(cw.output, "M="+result+"\n")
+}
+
+func (cw *CodeWriter) writeComparison(command string) {
+	cw.boolean += 1
+	comp := strings.ToUpper(command)
+	count := strconv.Itoa(cw.boolean)
+	io.WriteString(cw.output, "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n"+
+		"@"+comp+".true."+count+"\nD;J"+comp+"\n"+
+		"@SP\nA=M-1\nM=0\n@"+comp+".after."+count+"\n"+
+		"0;JMP\n("+comp+".true."+count+")\n@SP\nA=M-1\n"+
+		"M=-1\n("+comp+".after."+count+")\n")
 }
 
 // WritePushPop writes the assembly code that is the translation of the given command where command is either CPush or CPop.
